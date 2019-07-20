b9f32741519dc301ad4dc5ee62df26a10da71721
sameer aman <sameer.aman@milvik.se>
2019-07-20 19:12:28 +0530

    start

--- src/main/java/com/simplifiedsf/production/controllers/LinkRedirect.java
+++ src/main/java/com/simplifiedsf/production/controllers/LinkRedirect.java
@@ -7,8 +7,8 @@ public class LinkRedirect {
     @RequestMapping(value = "/test/{hash}")
     @ResponseBody
     public String link(@PathVariable String  hash){
-        // 1) verify hash was not made up
-        // 2) do whatever needs done with hash
+        // 1) verify hash was not made up /...  random100 length
+        // 2) do whatever needs to b done with hash
         return "somewhereElse";
     }
 }
